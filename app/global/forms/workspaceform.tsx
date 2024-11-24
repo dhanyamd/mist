51b2b48102f748046d2f0567ce3442c7c7bc1ba2
unknown <dhanya13md@gmail.com>
2024-11-24 13:37:33 +0530

    formd

--- app/global/forms/workspaceform.tsx
+++ app/global/forms/workspaceform.tsx
@@ -1,10 +1,23 @@
 import { useCreateWorkspace } from '@/app/hooks/useCreateWorkspace'
 import React from 'react'
+import FormGenerator from '../form-generator'
+import { Button } from '@/components/ui/button'
+import Loader from '@/app/(website)/_components/loader'
 
 const WorkspaceForm = () => {
     const {errors, isPending, onFormSubmit, register} = useCreateWorkspace()
   return (
     <form onSubmit={onFormSubmit} className='flex flex-col gap-y-3'>
+     <FormGenerator 
+              register={register}
+              placeholder='Workspace name'
+              label='Name'
+              errors={errors}
+              inputType='input'
+              type='text' name={'name'}     />
+                  <Button className='text-sm w-full mt-2' type='submit' disabled={isPending}>
+                    <Loader state={isPending}>Create workspace</Loader>
+                  </Button>
 
     </form>
   )
