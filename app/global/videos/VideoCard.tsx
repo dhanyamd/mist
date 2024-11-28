3e44eaae7a965509717d8ec4ad69e0df3fc16579
unknown <dhanya13md@gmail.com>
2024-11-29 00:48:54 +0530

    fix: width

--- app/global/videos/VideoCard.tsx
+++ app/global/videos/VideoCard.tsx
@@ -25,7 +25,11 @@ type Props = {
     workspaceId: string
   }
 const VideoCard = (props : Props) => {
-    const daysAgo = Math.floor(new Date().getTime() - props.createdAt.getTime()) / (24 * 60 * 60 * 1000)
+   // const daysAgo = Math.floor(new Date().getTime() - props.createdAt.getTime()) / (24 * 60 * 60 * 1000)
+   const daysAgo = Math.floor(
+    (new Date().getTime() - props.createdAt.getTime()) / (24 * 60 * 60 * 1000)
+  )
+
   return  <Loader
   className="bg-[#171717] flex justify-center items-center border-[1px] border-[rgb(37,37,37)] rounded-xl"
   state={props.processing}
@@ -57,18 +61,17 @@ const VideoCard = (props : Props) => {
           <h2 className='text-sm font-semibold text-[#BDBDBD]'>{props.title}</h2>
           <div className='flex gap-x-2 items-center mt-4'>
            <Avatar className='mt-2 w-8 h-8'>
-            <AvatarImage src={props.User?.image as string}>
+            <AvatarImage src={props?.User?.image as string }/>
             <AvatarFallback>
                 <User/>
             </AvatarFallback>
-            </AvatarImage>
            </Avatar>
           <div>
           <p className='capitalize text-[#BDBDBD] text-sm'>
             {props.User?.firstname} {props.User?.lastname}
           </p>
           <p className='text-[#6D6D6D]  text-xs flex items-center'>
-             <Dot/>    {daysAgo == 0 ? "Today" : `${daysAgo}d ago`}
+             <Dot/>  {daysAgo == 0 ? "Today" : `${daysAgo}d ago`}
           </p>
           </div>
           </div>
